fe57deea075549262c0ab0d1749588c3a0c5cae8
Mr_Garfield <1875646441@qq.com>
2018-08-29 19:40:00 +0800

    LeetCode
    数组最长连续递增子序列

---
+++ findLengthOfLCIS.c
@@ -0,0 +1,19 @@
+﻿// Copyright 2018 Cocoon Psn.
+// Author: Mr_Garfield
+// LeetCode674 Longest continous increasing subsequence
+ 
+int findLengthOfLCIS(vector<int>& nums) {
+    if(nums.size() == 0)
+        return 0;
+    int indexLength = 1,maxLength = 1, n = nums.size();
+    for (int i = 1; i < n; ++i) {
+        if (nums[i] > nums[i-1]) 
+        {
+            indexLength++;
+            maxLength = max(maxLength, indexLength);
+        }
+        else 
+            indexLength = 1;
+    }
+    return maxLength;
+}
