bc0e3d3173464e31501261555e6df3aa6de40de0
Mr_Garfield <1875646441@qq.com>
2018-08-30 22:57:33 +0800

    小偷抢街
    LeetCode198

---
+++ robStreet.c
@@ -0,0 +1,32 @@
+﻿// Copyright 2018 Cocoon Psn.
+// Author: Mr_Garfield
+// Description：LeetCode198 小偷抢街
+
+vector<int> result;    
+int max(int a, int b)
+{
+    int max = a;
+    if(b>a)
+        max = b;
+    return max;
+}
+int search(vector<int>& nums, int idex)
+{
+    if(idex < 0)
+        return 0;
+    if(result[idex] >=0 )
+        return result[idex];
+    result[idex] = max(nums[idex]+search(nums, idex-2),search(nums, idex-1));
+    return result[idex];
+}
+    
+    
+int rob(vector<int>& nums) {
+    int len = nums.size();
+    result.resize(len);
+    for(int i = 0; i < len; i++)
+    {
+        result[i] = -1;
+    }
+    return search(nums, len-1);
+}
